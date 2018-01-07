647a3e734fbd1289f567749596b4ed8785194df0
Igor Rafael de Sousa <igorrafaeldesousa@gmail.com>
2018-01-06 20:04:12 -0500

    Move BoneTracker.TrackedBone to TransformMatcher

--- script/BoneTracker.cs
+++ script/BoneTracker.cs
@@ -6,43 +6,13 @@ using UnityEngine;
 [Serializable]
 public class BoneTracker
 {
-    [Serializable]
-    public class TrackedBone
-    {
-        [SerializeField]
-        private List<string> _path = new List<string>();
-
-        public TrackedBone(Transform rootBone, Transform bone)
-        {
-            while (rootBone != bone)
-            {
-                if (bone == null)
-                {
-                    throw new Exception();
-                }
-
-                _path.Add(bone.name);
-                bone = bone.parent;
-            }
-            _path.Reverse();
-        }
-
-        internal Transform FindMatch(Transform root)
-        {
-            return _path.Aggregate(root, (a, b) => a.Find(b));
-        }
-    }
-
     [SerializeField]
-    private SkinnedMeshRenderer _skinnedMeshRenderer;
-    [SerializeField]
-    private List<TrackedBone> _bones;
+    private List<TransformMatcher> _bones;
 
     public BoneTracker(SkinnedMeshRenderer skinnedMeshRenderer)
     {
-        _skinnedMeshRenderer = skinnedMeshRenderer;
         Transform root = skinnedMeshRenderer.rootBone;
-        _bones = skinnedMeshRenderer.bones.Select(b => new TrackedBone(root, b)).ToList();
+        _bones = skinnedMeshRenderer.bones.Select(b => new TransformMatcher(root, b)).ToList();
     }
 
     internal Transform[] Match(Transform root)
---
+++ script/TransformMatcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[Serializable]
+public class TransformMatcher
+{
+    [SerializeField]
+    private List<string> _path = new List<string>();
+
+    public TransformMatcher(Transform rootBone, Transform bone)
+    {
+        while (rootBone != bone)
+        {
+            if (bone == null)
+            {
+                throw new Exception();
+            }
+
+            _path.Add(bone.name);
+            bone = bone.parent;
+        }
+        _path.Reverse();
+    }
+
+    internal Transform FindMatch(Transform root)
+    {
+        return _path.Aggregate(root, (a, b) => a.Find(b));
+    }
+}
